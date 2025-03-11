b00287a16718c73ac4dc428aaf4ad5fb5db90416
lena <lenarauj@mit.edu>
2025-03-10 20:10:26 -0400

    Update parameterization.m

--- parameterization.m
+++ parameterization.m
@@ -1,7 +1,7 @@
 % "Challenge: Make your own visualization that illustrates parameterization of surfaces using some software or programming language."
 % matlab has built in f'ns for parametric surfaces
 
-#% i did a hyperbolic paraboloid surface below 
+% i did a hyperbolic paraboloid surface below 
 
 % u and v are 3d matrices representing grid values 
 [u,v] = meshgrid(-2:0.1:2, -2:0.1:2);
